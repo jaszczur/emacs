d0b605149d01bfbd08ae330ec2dd17226ff4dc53
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-03 16:10:15 +0100

    evil and eglot updates

--- init.el
+++ init.el
@@ -1,4 +1,13 @@
+;;; init.el --- loader for org-babel configuration
+
+;;; Commentary:
+;; Just load config.org using org-babel-load-file.
+
+;;; Code:
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
+;;; init.el ends here
