cd66941bc27b43158506b1f3796c3ed7fb15963e
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-03 19:01:19 +0100

    disable native compilation
    Using Emacs 29.4 from Nix unstable fixes old seq dependency, but puts
    lots of warnings about inability to load libgccjit and native
    compile. For now let's just disable nativecomp - it's fast enough
    without it.

--- early-init.el
+++ early-init.el
@@ -1,2 +1,5 @@
+;; Disable package loading - we'll use Elpaca
 (setq package-enable-at-startup nil)
 
+;; Disable native compilation - it doesn't work on Mac with Nix
+(setq native-comp-deferred-compilation nil)
