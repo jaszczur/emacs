f05ec6a096b716f0cd07e57251af479eda8a10c8
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-12-06 09:12:11 +0100

    reenable native comp after changes in env

--- early-init.el
+++ early-init.el
@@ -2,4 +2,4 @@
 (setq package-enable-at-startup nil)
 
 ;; Disable native compilation - it doesn't work on Mac with Nix
-(setq native-comp-deferred-compilation nil)
+;; (setq native-comp-deferred-compilation nil)
